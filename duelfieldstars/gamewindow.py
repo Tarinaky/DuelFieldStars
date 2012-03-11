bb59bdce5a2e92a10246e3aeefd3bb8bdd1034dc
Tarinaky <Tarinaky@gmail.com>
2012-03-11 02:17:07 +0000

    Attempting to get gameViewport to draw using custom method. Currently fails without error.

--- duelfieldstars/gamewindow.py
+++ duelfieldstars/gamewindow.py
@@ -7,9 +7,20 @@ from PySide import QtGui, QtUiTools, QtCore
 class GameWindow(QtGui.QWidget):
     def __init__(self):
         super(GameWindow, self).__init__()
+        
+        # Load ui file.
         loader = QtUiTools.QUiLoader()
         file_ = QtCore.QFile("forms/GameWindow.ui")
         file_.open(QtCore.QFile.ReadOnly)
-        myWidget = loader.load(file_,self)
+        self.widget = loader.load(file_,self)
     
-        myWidget.show()
+        # Overload repaint for viewport.
+        def paintEvent(self,event):
+            painter = QtGui.QPainter(self)
+            painter.fillRect(self.rect(), QtGui.QBrush(QtCore.Qt.red, QtCore.Qt.Dense2Pattern) )
+            return
+        self.widget.gameViewport.paintEvent = paintEvent
+    
+        # Show and perform initial draw.    
+        self.widget.show()
+        
