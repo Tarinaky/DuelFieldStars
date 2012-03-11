3c323c56e3286686f8a5c56bccf149d79dbea8e7
Tarinaky <Tarinaky@gmail.com>
2012-03-11 01:14:16 +0000

    Fixed warning, assignment to built-in 'file'.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
         
     loader = QtUiTools.QUiLoader()
-    file = QtCore.QFile("forms/GameWindow.ui")
-    file.open(QtCore.QFile.ReadOnly)
+    file_ = QtCore.QFile("forms/GameWindow.ui")
+    file_.open(QtCore.QFile.ReadOnly)
     myWidget = loader.load(file)
     
     myWidget.show()
