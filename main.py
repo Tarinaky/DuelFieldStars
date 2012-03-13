35a23754cbdb7622a3d88503eed1b10f04dc049c
Tarinaky <Tarinaky@gmail.com>
2012-03-13 17:12:57 +0000

    Removed all Qt code from project.

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 import sys
 
-from PySide import QtUiTools, QtCore, QtGui
-
-from duelfieldstars import gamewindow
-
 if __name__ == '__main__':
-    app = QtGui.QApplication(sys.argv)
-    myWindow = gamewindow.GameWindow()
-    print myWindow
-    app.exec_()
+    print "TODO: Implement"
