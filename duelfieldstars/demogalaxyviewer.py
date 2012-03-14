f576654a5c49c5629be99d92955b7fabbdd02bfd
Tarinaky <Tarinaky@gmail.com>
2012-03-14 23:52:37 +0000

    Added vertical scrollbar.

--- duelfieldstars/demogalaxyviewer.py
+++ duelfieldstars/demogalaxyviewer.py
@@ -5,7 +5,7 @@ from ui_abstract.window import Window
 from ui_abstract.widget import Widget
 
 from viewportwidget import ViewportWidget
-from scrollbars import HorizontalScrollBar
+from scrollbars import HorizontalScrollBar, VerticalScrollBar
 
 from model.galaxy import Galaxy
 
@@ -21,6 +21,8 @@ class GalaxyViewerWindow(Window):
         self.add_widget(self.viewport)
         self.hzScrollbar = HorizontalScrollBar(pygame.Rect(0,self.height-8,self.width,self.height), self.viewport)
         self.add_widget(self.hzScrollbar, False)
+        self.vrScrollbar = VerticalScrollBar(pygame.Rect(self.width-8,0,self.width,self.height), self.viewport)
+        self.add_widget(self.vrScrollbar, False)
         
         return
   
--- duelfieldstars/scrollbars.py
+++ duelfieldstars/scrollbars.py
@@ -43,3 +43,43 @@ class HorizontalScrollBar(Bar):
         
         self.surface.fill(fgColor, pygame.Rect(x, 0, blockWidth, blockHeight)) 
         
+class VerticalScrollBar(Bar):
+    """A widget for a scrollbar to go along the bottom of the viewport."""
+    def __init__(self,rect,viewport):
+        """
+        rect is the bounding box for this widget on the window.
+        viewport is a viewport widget to control.
+        """
+        super(VerticalScrollBar,self).__init__(rect)
+        
+        self.viewport = viewport
+                
+        self.add_mouse_handler(self.jump, pygame.MOUSEBUTTONDOWN, 1)
+        
+    def jump(self):
+        """Called when the scrollbar is clicked on, changes the value of the scrollbar."""
+        
+        (_,y) = pygame.mouse.get_pos()
+        y = y * self.max / self.height - self.blockHeight
+        (x,_) = self.viewport.position
+        
+        self.viewport.position = (x,y)
+
+    def on_tick(self, deltaTime):
+        self.set_max(self.viewport.galaxy.height * self.viewport.scale)
+        self.blockHeight = self.height**2/self.max
+        (_,y) = self.viewport.position
+        self.set_value(y)
+
+    def on_draw(self):
+        bgColor = (64,64,64)
+        fgColor = (0,0,255)
+        
+        self.surface.fill(bgColor)
+        
+        y = self.value * self.height / self.max
+        
+        blockHeight = self.blockHeight
+        blockWidth = self.width
+        
+        self.surface.fill(fgColor, pygame.Rect(0, y, blockWidth, blockHeight)) 
