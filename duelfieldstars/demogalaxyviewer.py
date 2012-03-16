23a9aef1504ba195f9a0e7877958c430f55377e8
Tarinaky <tarinaky@gmail.com>
2012-03-16 14:00:56 +0000

    Attempted to add a bar to the top of the display. Currently throws an exception.

--- duelfieldstars/demogalaxyviewer.py
+++ duelfieldstars/demogalaxyviewer.py
@@ -1,13 +1,17 @@
 import pygame
 import logging
+import sys
 
 from ui.ui_abstract.window import Window
 
 from ui.viewportwidget import ViewportWidget
 from ui.scrollbars import HorizontalScrollBar, VerticalScrollBar
 from ui.planetdetails import PlanetDetails
+from ui.ticker import Ticker
 
 from model.galaxy import Galaxy
+
+log = logging.getLogger(__name__)
 
 screenResolution = (640,480)
 
@@ -25,10 +29,13 @@ class GalaxyViewerWindow(Window):
         self.hzScrollbar = HorizontalScrollBar(pygame.Rect(0,self.height-8,width-174,8), self.viewport)
         self.add_widget(self.hzScrollbar, False)
         
-        self.vrScrollbar = VerticalScrollBar(pygame.Rect(width-8-174,0,8,height), self.viewport)
+        self.vrScrollbar = VerticalScrollBar(pygame.Rect(width-8-174,14,8,height-14), self.viewport)
         self.add_widget(self.vrScrollbar, False)
         
         self.detailsPanel = None
+
+        self.ticker = Ticker(pygame.Rect(0,0,self.width-174,14) )
+        self.add_widget(self.ticker,False)
         
         return
     
@@ -47,6 +54,8 @@ if __name__ == '__main__':
     pygame.display.set_mode(screenResolution)
     window = GalaxyViewerWindow()
     window.run()
+    
+    log.debug("Quitting...")
     
         
         
--- duelfieldstars/model/planet.py
+++ duelfieldstars/model/planet.py
@@ -12,15 +12,15 @@ class Planet(object):
     def __init__(self, *position):
         self.name = name.name()
         
-        self.baseValue = None # The planet's base value expressed as a percentile.
-        self.currentValue = None # The planet's value after terraforming.
-        self.realisedValue = None # The planet's presently realised value expressed as a percentile.
-        self.growth = None # The planet's percentage growth per turn
+        self.baseValue = 0 # The planet's base value expressed as a percentile.
+        self.currentValue = 0 # The planet's value after terraforming.
+        self.realisedValue = 0 # The planet's presently realised value expressed as a percentile.
+        self.growth = 0 # The planet's percentage growth per turn
         
         self.improvementLevels = [] # The planet's five mining improvement levels.
         self.realisedImprovement = 0 
         
-        self.type = None # The planet's type, expressed as one of the letters A, B, C, D or E.
+        self.type = '' # The planet's type, expressed as one of the letters A, B, C, D or E.
         
         self.position = position # The planet's position expressed as an (x,y) tuple.
         return
@@ -48,6 +48,10 @@ class Planet(object):
                 income += 1
         return income
 
+    def tick(self):
+        """Update the planet by 1 turn."""
+        self.realisedValue += self.growth
+
 class PlanetTest(unittest.TestCase):
     def setUp(self):
         self.fixture = Planet(0,0)
@@ -56,3 +60,5 @@ class PlanetTest(unittest.TestCase):
     def test_generation(self):
         self.fixture.generate(random.Random(0xDEADBEEF) )   
         return
+
+NOPLANET = Planet()
