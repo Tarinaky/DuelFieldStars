fc352eda118e875df9c7d646453ade03e15b04f5
Tarinaky <Tarinaky@gmail.com>
2012-03-20 12:44:42 +0000

    Added more methods to menu interface.

--- duelfieldstars/ui/ui_abstract/menu.py
+++ duelfieldstars/ui/ui_abstract/menu.py
@@ -5,17 +5,22 @@ from widget import Widget
 class Menu (Widget):
     """
     Class defining a text menu of options that a user may select and execute.
-    NOTE: This interface contains no information how to display the menu options.
-    
-    See also: ListMenu, IconMenu
     """
     def __init__(self,rect):
         super(Menu,self).__init__(rect)
         
         self.options = [] # A list of (widget,method,*arg) tuples.
         
+        self.add_mouse_handler(self.click, pygame.MOUSEBUTTONDOWN, 1)
+        self.add_mouse_handler(self.click, pygame.MOUSEBUTTONDOWN, 3)
+        
     def add_option(self, widget, method, *arguments):
         self.options.append((widget,method,arguments))
+        
+    def _tick(self,deltaTime):
+        super(Menu,self)._tick(deltaTime)
+        for (widget,_,_) in self.options:
+            widget._tick(deltaTime)
         
     def click(self,button):
         (mouseX, mouseY) = pygame.mouse.get_pos()
@@ -24,3 +29,8 @@ class Menu (Widget):
             if widget.rect.collidepoint(mouseX, mouseY):
                 method(*args)
                 
+    def _draw(self):
+        super(Menu,self)._draw()
+        
+        for (widget, _, _) in self.options:
+            self.surface.blit(widget.surface, widget.x0, widget.y0)
