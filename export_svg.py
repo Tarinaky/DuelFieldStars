49e8cd0ce0e05ebfd574eabbea9d10ccaf27291c
Tarinaky <Tarinaky@gmail.com>
2012-06-16 00:08:07 +0100

    Exported graphics as bitmaps. Hurray.

--- export_svg.py
+++ export_svg.py
@@ -1,14 +1,19 @@
 import os
+import string
 
 def export_svg():
     for file in os.listdir("svg"):
         if os.name == 'nt':
-            command = "c:\Program Files\Inkscape\Inkscape.exe"
+            command = '"c:\Program Files\Inkscape\Inkscape.exe"'
         else:
             command = "inkscape"
         for i in [2**a for a in range(3,8)]:
-            os.system(command+"-zf "+file+" -C --export-width="+i+" --export-height="+i)
+            print "Exporting "+file+" at size "+str(i)
+            invocation = command+" -f svg/"+file+" -e asset/"+file.replace('.svg','_'+str(i)+'.png')+" --export-width="+str(i)+" --export-height="+str(i)
+            print invocation
+            os.system(invocation)
     
-
+    
 if __name__ == '__main__':
     export_svg()
+    print os.getcwd()
