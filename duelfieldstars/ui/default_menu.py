99df06a2d1cf12a37986dc57bd829dd7be9c3cf8
Tarinaky <tarinaky@gmail.com>
2012-06-21 01:03:17 +0100

    Menus now 'snap' to the edge of the screen so that all options are always visible.

--- duelfieldstars/ui/default_menu.py
+++ duelfieldstars/ui/default_menu.py
@@ -24,3 +24,26 @@ class DefaultMenu(Menu):
         self.rect.width = self.surface.get_width()
         self.rect.height = self.surface.get_height() 
         self.surface.fill(COLORS["darkGray"])
+        
+        # Check that this surface is completely within the display,
+        # else nudge it
+        def check_rect_in_display(rect):
+            (x0,y0,w,h) = (rect.left, rect.top, rect.width, rect.height)
+            display_width = pygame.display.get_surface().get_width()
+            display_height = pygame.display.get_surface().get_height()
+            
+            if x0 < 0:
+                x0 = 0
+            if y0 < 0:
+                y0 = 0
+            if x0+w > display_width:
+                x0 = display_width - w
+            if y0+h > display_height:
+                y0 = display_height - h
+            
+            (rect.left, rect.top, rect.width, rect.height) = (x0,y0,w,h)
+            return rect
+        
+        self.rect = check_rect_in_display(self.rect)
+                
+            
