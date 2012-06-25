3452fba3eac2fb4de2a838c1ea26f701b7684168
Tarinaky <tarinaky@gmail.com>
2012-06-25 19:56:27 +0100

    Image rect's width and height are set from the surface passed in.

--- duelfieldstars/ui/ui_abstract/image.py
+++ duelfieldstars/ui/ui_abstract/image.py
@@ -5,6 +5,8 @@ from ui.ui_abstract.widget import Widget
 
 class Image(Widget):
     def __init__(self,rect,surface):
+        rect.w = surface.get_width()
+        rect.h = surface.get_height()
         super(Image,self).__init__(rect)
         self.surface = surface
     def on_draw(self):
