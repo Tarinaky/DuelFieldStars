732c0fda225c8a899088f8b1b50bf42217db1d08
Tarinaky <tarinaky@gmail.com>
2012-06-26 16:24:00 +0100

    List of all worlds works, but is not yet interactive.

--- duelfieldstars/ui/ledger_all_worlds.py
+++ duelfieldstars/ui/ledger_all_worlds.py
@@ -6,6 +6,8 @@ from ui.ui_abstract.widget import Widget
 from ui.ui_abstract.text import Text
 import pygame
 from color import COLORS
+from model import game
+from ui import texture_cache
 
 class LedgerAllWorlds(Widget):
     ALL = object()
@@ -54,15 +56,82 @@ class LedgerAllWorlds(Widget):
             def clicked():
                 self.show = self.ALL
                 self.update()
-            self.elements.append((widget,all_worlds_button,[]))
-            x += widget.width+10
+            self.elements.append((widget,clicked,[]))
             return (widget.width, widget.height)
         (dx,dy) = all_worlds_button(x,y)
         x += dx *1.5
+        
+        # Filter friendly worlds
+        def friend_worlds_button(x,y):
+            widget = Text(pygame.Rect(x,y,0,0), font, COLORS["green"],
+                          "FRIEND")
+            def clicked():
+                self.show = self.FRIEND
+                self.update()
+            self.elements.append((widget,clicked,[]))
+            return (widget.width, widget.height)
+        (dx,dy) = friend_worlds_button(x,y)
+        x += dx *1.5
+        
+        # Filter enemy worlds
+        def foe_worlds_button(x,y):
+            widget = Text(pygame.Rect(x,y,0,0), font, COLORS["red"],
+                          "FOE")
+            def clicked():
+                self.show = self.FOE
+                self.update()
+            self.elements.append((widget, clicked, []))
+            return (widget.width, widget.height)
+        (dx,dy) = foe_worlds_button(x, y)
+        x = 0
+        dy += dy *1.5
+        
+        self.list_start = dy # Start list from here.
         
         
     def on_draw(self):
         for (widget,_,_) in self.elements:
             self.surface.blit(widget.surface, widget.rect)
-            
         
+        # Draw list.
+        y = self.list_start
+        for world in game.galaxy.planets.values():
+            if world.owner == None:
+                pass
+            elif self.show == self.FOE and world.owner == game.factions[0]:
+                # Hide friends when filtering for foes.
+                continue
+            elif self.show == self.FRIEND and world.owner != game.factions[0]:
+                # Hide foes when filtering for friends.
+                continue
+            dy = 0
+            # Planet name
+            if world.owner == game.factions[0]:
+                color = COLORS["green"]
+            else:
+                color = COLORS["red"]
+            string = world.name+" "+str(world.position)
+            texture = texture_cache.text(None, 12, color, string)
+            self.surface.blit(texture,(0,y+dy))
+            dy += texture.get_height()
+            
+            # Income
+            try:
+                string = "    Income: "+str(world.income)+" rez/turn"
+            except:
+                string = "    No owner"
+            texture = texture_cache.text(None,12,COLORS["white"],string)
+            self.surface.blit(texture,(0,y+dy))
+            dy += texture.get_height()
+            
+            # Construction
+            string = "    Building: "+str(world.construction)
+            texture = texture_cache.text(None,12,COLORS["white"],string)
+            self.surface.blit(texture,(0,y+dy))
+            dy += texture.get_height()
+            
+            # Next entry
+            y += dy
+            self.tile_height = dy
+            
+            
