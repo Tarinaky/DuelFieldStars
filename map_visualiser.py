9aa4b3b44ee451795f180edc176a9a6cc4089158
Tarinaky <tarinaky@gmail.com>
2013-10-11 20:12:23 +0100

    Overloaded resizeGL to set up projection and added logarithmic slider.

--- map_visualiser.py
+++ map_visualiser.py
@@ -14,6 +14,12 @@ class ViewWidget(QGLWidget):
         super(ViewWidget, self).__init__()
         self.field = {}
 
+    def resizeGL(self, w,h):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glViewport(0, 0, w, h)
+        gluPerspective(90, float(w)/h, 0.1, 1000)
+
     def loadHabHYG(self):
         with open("./data/HabHYG.csv", "rb") as csvfile:
             reader = csv.reader(csvfile)
@@ -25,24 +31,39 @@ class ViewWidget(QGLWidget):
                     name = row[3]
                     self.field[(x,y,z)] = name
                 except:
-                    pass
+                    print "Not a valid row: "+str(row)
 
 
     def paintGL(self):
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        gluLookAt(0, 0, 10**(self.zoom.value() ),
+                0,0,0,
+                0,1,0)
+
+        print "Zoom: "+str(10**(self.zoom.value() ) )
+
         glColor(1,1,1)
-        glPointSize(16)
+        glPointSize(1)
         glBegin(GL_POINTS)
         for (x,y,z) in self.field:
             glVertex(x,y,z)
         glEnd()
+
+        self.zoom.repaint()
 
 
 
 class MapVisualiser(object):
     def __init__(self):
         self.ui = ViewWidget()
+        self.ui.resize(QtCore.QSize(640, 480) )
         self.ui.loadHabHYG()
+
+        self.ui.zoom = QtGui.QSlider(QtCore.Qt.Orientation.Vertical, parent=self.ui)
+        self.ui.zoom.valueChanged.connect(self.ui.paintGL)
 
     def publish(self):
         self.ui.show()
